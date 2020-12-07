599165065c234f05d0f7ccff06555564c4eaffa4
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-07 15:41:33 -0600

    Perform npm version command

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const core = require('@actions/core')
 const { exec } = require('child_process')
+const fs = require('fs')
 
 const versions = ['major', 'minor', 'patch']
 
 const version = core.getInput('version')
-if (!versions.includes(version)) {
+if (versions.includes(version)) {
+    const command = `npm version ${version}`
+    core.info(`Running command: ${command}`)
+    exec(command).once('exit', code => {
+        if (!code) {
+            core.info(JSON.parse(fs.readFileSync('package.json')))
+        } else {
+            core.setFailed(`Command exited with code: ${code}`)
+        }
+    })
+
+} else {
     core.setFailed('Invalid version. Use either major, minor, or patch.')
 }
-console.log('this ran')
