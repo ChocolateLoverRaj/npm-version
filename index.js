6d530190b97f3abc5728c73a6f08786857649da6
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-07 15:44:13 -0600

    logging

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const version = core.getInput('version')
 if (versions.includes(version)) {
     const command = `npm version ${version}`
     core.info(`Running command: ${command}`)
-    exec(command).once('exit', code => {
+    const update = exec(command).once('exit', code => {
         if (!code) {
             core.info(JSON.parse(fs.readFileSync('package.json')))
         } else {
             core.setFailed(`Command exited with code: ${code}`)
         }
     })
-
+    update.stderr.pipe(process.stderr)
+    console.log(process.cwd(), fs.readdirSync(process.cwd()))
 } else {
     core.setFailed('Invalid version. Use either major, minor, or patch.')
 }
