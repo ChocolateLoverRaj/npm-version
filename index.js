f340fbb232acf4257e52bb60f5d664b828820555
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-07 16:13:23 -0600

    Create blob

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const versions = ['major', 'minor', 'patch']
 const version = core.getInput('version')
 if (versions.includes(version)) {
     core.info('Updating package version')
-    update().then(version => {
-        core.info(`Updated package version to ${version}`)
-    })
+    update()
 } else {
     core.setFailed('Invalid version. Use either major, minor, or patch.')
 }
@@ -24,6 +22,7 @@ async function update() {
         core.setFailed('Error reading package.json')
         return
     }
+
     core.info('Parse package.json')
     try {
         packageJson = JSON.parse(packageJson)
@@ -31,6 +30,7 @@ async function update() {
         core.error(e)
         core.setFailed('Error reading package.json')
     }
+
     core.info(`Old version: ${packageJson.version}`)
     const semver = packageJson.version.split('.')
     const versionPosition = versions.indexOf(version)
@@ -38,5 +38,25 @@ async function update() {
     for (let i = versionPosition + 1; i < versions.length; i++) {
         semver[i] = '0'
     }
-    core.info(`New version: ${semver.join('.')}`)
+    const newVersion = semver.join('.')
+    core.info(`New version: ${newVersion}`)
+    packageJson.version = newVersion
+
+    core.info('Create new package.json')
+    const octokit = github.getOctokit(core.getInput('token'))
+    let sha
+    try {
+        const blob = await octokit.git.createBlob({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            content: JSON.stringify(packageJson, undefined, 2),
+            encoding: 'utf-8'
+        })
+        sha = blob.data.sha
+    } catch (e) {
+        core.error(e)
+        core.setFailed('Error creating blob')
+        return
+    }
+    core.info(sha)
 }
