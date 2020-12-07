361b3ff711df3dc01ef68c84097d578fb86faf69
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-07 15:14:41 -0600

    pipe stderr

--- install.js
+++ install.js
@@ -2,7 +2,9 @@ const { exec } = require('child_process')
 
 const install = exec('npm ci')
     .once('exit', code => {
+        console.log('Mission failed')
         process.exit(code)
     })
 
 install.stdout.pipe(process.stdout)
+install.stderr.pipe(process.stderr)
