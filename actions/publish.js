57e9da11361d0132371731eaaa6abafe35114afa
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-09 13:20:29 -0600

    npm login first

--- actions/publish.js
+++ actions/publish.js
@@ -1,24 +1,27 @@
 const core = require('@actions/core')
 const { exec } = require('child_process')
 
-module.exports = () => new Promise((resolve, reject) => {
-    console.log('Publishing package')
-    const publish = exec('npm publish')
+const runCommand = name => new Promise((resolve, reject) => {
+    const command = exec(name)
     let err = ''
-    publish.stdout.on('data', data => {
+    command.stdout.on('data', data => {
         err += data
     })
-    publish.stderr.on('data', data => {
+    command.stderr.on('data', data => {
         err += data
     })
-    publish.on('exit', code => {
+    command.on('exit', code => {
         if (!code) {
-            core.info('Successfully published package')
             resolve()
         } else {
-            core.error(err)
-            core.error('Error publishing')
             reject(new Error(err))
         }
     })
 })
+
+module.exports = async () => {
+    core.info('Login to npm')
+    await runCommand('npm login')
+    core.info('Publish package')
+    await runCommand('npm publish')
+}
