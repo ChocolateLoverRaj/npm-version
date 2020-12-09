acf4c857d9ce706615e9414d55a979be6024eff8
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-09 13:03:19 -0600

    Actually publishes

--- actions/publish.js
+++ actions/publish.js
@@ -1,4 +1,24 @@
-module.exports = () => {
-    console.log('Will publish soon')
-    console.log(process.env.NODE_AUTH_TOKEN.length)
-}
+const core = require('@actions/core')
+const { exec } = require('child_process')
+
+module.exports = () => new Promise((resolve, reject) => {
+    console.log('Publishing package')
+    const publish = exec('npm publish')
+    let err = ''
+    publish.stdout.on('data', data => {
+        err += data
+    })
+    publish.stderr.on('data', data => {
+        err += data
+    })
+    publish.on('exit', code => {
+        if (!code) {
+            core.info('Successfully published package')
+            resolve()
+        } else {
+            core.error(err)
+            core.error('Error publishing')
+            reject(new Error(err))
+        }
+    })
+})
