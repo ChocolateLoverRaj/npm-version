76cd487813bc114a91203636d9ecbc1c1e0a10f5
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-09 07:29:19 -0600

    no token for publish

--- publish/index.js
+++ publish/index.js
@@ -1,10 +1,3 @@
 const publish = require('../actions/publish')
-const core = require('@actions/core')
-const github = require('@actions/github')
-const octokit = github.getOctokit(core.getInput('token'))
 
-publish({
-    github: github,
-    octokit: octokit,
-    getInput: core.getInput
-})
+publish()
