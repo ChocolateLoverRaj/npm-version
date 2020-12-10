197f517af5245d81f885a5208008532578ba57b0
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-09 19:12:28 -0600

    Release

---
+++ actions/release.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core')
+const { promise: { readFile } } = require('fs')
+
+module.exports = async ({ octokit, github }) => {
+    core.info('Read package.json')
+    const packageJsonRaw = readFile('package.json')
+    core.info('Parse package.json')
+    const packageJson = JSON.parse(packageJsonRaw)
+    const versionRaw = packageJson.version
+    core.info('Parse version')
+    const version = parseInt(versionRaw)
+    core.info(`Release version: ${version}`)
+    await octokit.repos.createRelease({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        tag_name: version,
+        target_commitish: github.context.payload.repository.default_branch
+    })
+}
