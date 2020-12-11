8f81eba8eed0ba2d312452fcb83f2b5e4316dd68
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-11 15:39:24 -0600

    Log package shasum

--- check/index.js
+++ check/index.js
@@ -1,7 +1,23 @@
 const core = require('@actions/core')
 const { exec } = require('child_process')
+const { createInterface } = require('readline')
+
+const shasumText = 'npm notice shasum:'
 
 core.info('Running npm publish --access public --dry-run')
 const publish = exec('npm publish --access publish --dry-run')
-publish.stdout.pipe(process.stdout)
-publish.stderr.pipe(process.stderr)
+const publishOutput = createInterface({ input: publish.stdout })
+let shasum
+publishOutput.on('line', line => {
+    if (line.startsWith(shasumText)) {
+        shasum = line.slice(shasumText.length).trim()
+        core.info(`Package shasum: ${shasum}`)
+    }
+})
+publish.on('exit', code => {
+    if (!code) {
+
+    } else {
+        core.error('Error getting shasum')
+    }
+})
