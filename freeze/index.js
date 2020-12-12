a2d31d1a31f26cea12caa1d1136a67b76be1faf3
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-12 10:19:49 -0600

    Utf8

--- freeze/index.js
+++ freeze/index.js
@@ -32,12 +32,12 @@ const checkFreeze = (previousJson, json, freeze) => {
 const start = async () => {
     const jsonPath = core.getInput('options')
     core.info(`Reading json file: ${jsonPath}`)
-    const jsonRaw = await readFile(jsonPath)
+    const jsonRaw = await readFile(jsonPath, 'utf8')
     core.info('Parsing json')
     const json = JSON.parse(jsonRaw)
     core.info('Checking previous json file')
     const previousJson = await getRaw(jsonPath)
-    if (previousJson !== null && previousJson.trim() !== jsonRaw.trim()) {
+    if (previousJson !== null && previousJson !== jsonRaw) {
         throw new Error('Json file was modified. Ignore this check if this change was intended.')
     }
     core.info('Validating json')
