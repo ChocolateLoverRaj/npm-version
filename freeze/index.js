f995e8a68acc6fc718506009fcf6c8abea61aa8f
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-12 08:07:40 -0600

    get previous json

--- freeze/index.js
+++ freeze/index.js
@@ -1,14 +1,32 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const { Octokit } = require('@octokit/rest')
 const { Validator } = require('jsonschema')
 const { promises: { readFile } } = require('fs')
 
 const start = async () => {
+    const token = core.getInput('token')
+    let octokit
+    if (token) {
+        core.info('Getting octokit with token')
+        octokit = github.getOctokit(token)
+    } else {
+        core.info('Getting octokit without token')
+        octokit = new Octokit()
+    }
     const jsonPath = core.getInput('options')
     core.info(`Reading json file: ${jsonPath}`)
     const jsonRaw = await readFile(jsonPath)
     core.info('Parsing json')
     const json = JSON.parse(jsonRaw)
+    core.info('Checking previous json file')
+    const previousJson = await octokit.repos.getContent({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        path: jsonPath,
+        ref: github.context.payload.pull_request.base.ref
+    })
+    console.log(previousJson.data)
     core.info('Validating json')
     const v = new Validator()
     v.validate(json, {
