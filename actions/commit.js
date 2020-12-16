ca53881dcf9164492a8f1cfb627dc74d5e84ef42
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-16 07:02:33 -0600

    Log last tree

--- actions/commit.js
+++ actions/commit.js
@@ -28,7 +28,7 @@ module.exports = async ({ github, octokit, getInput }) => {
             tree_sha: lastCommit.data.tree.sha,
             recursive: recursive
         })
-        console.log(lastTree)
+        console.log(lastTree.data.tree)
     }
     const blobs = (await Promise.all(files.map(async file => {
         const blob = await octokit.git.createBlob({
